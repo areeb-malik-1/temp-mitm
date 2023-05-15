3d8870f9d643cbc6aef6fc1b060ee6f63d35ee9a
Areeb Malik <areeb.malik@tiket.com>
2023-05-15 23:37:59 +0530

    update

--- src/main/java/Temp.java
+++ src/main/java/Temp.java
@@ -1,7 +1,21 @@
+import io.appium.mitmproxy.InterceptedMessage;
 import io.appium.mitmproxy.MitmproxyJava;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.concurrent.TimeoutException;
+
 public class Temp {
-    public static void main(String[] args) {
-        MitmproxyJava m = new MitmproxyJava()
+    public static void main(String[] args) throws IOException, URISyntaxException, TimeoutException, InterruptedException {
+        MitmproxyJava proxy = new MitmproxyJava("/home/mitmdump", (InterceptedMessage m) -> {
+            System.out.println("intercepted request for " + m.requestURL.toString());
+            return m;
+        });
+
+        proxy.start();
+
+        Thread.sleep(2 * 60 * 1000);
+
+        proxy.stop();
     }
 }
